ee3bb4c16aed7e6ce6e14d7541f26fa268a85709
yoer <iamyoer@gmail.com>
2017-08-19 21:54:06 +0800

    sql in's query bug

--- source/src/generator/element/qrsqlin.cpp
+++ source/src/generator/element/qrsqlin.cpp
@@ -14,7 +14,7 @@ QString QrSqlIn::toString() const
 {
     Q_ASSERT(1 == listColumns.count() && 1 == listKeys.count() && listValues.count() >= 1);
     QString statement, column(listColumns.at(0)), key(listKeys.at(0));
-    bool questionMark = (QrSqlConfig::getInstance ()->getPlaceholderStyle() == QrSqlConfig::ph_style_question_mark);
+//    bool questionMark = (QrSqlConfig::getInstance ()->getPlaceholderStyle() == QrSqlConfig::ph_style_question_mark);
     Q_ASSERT(! column.isEmpty() && ! key.isEmpty());
 
     switch (type)
@@ -28,14 +28,15 @@ QString QrSqlIn::toString() const
 
     for (int i = 0; i < listValues.count(); i++)
     {
-       if ((type == in_select) || (type == not_in_select)) {
-           statement += listValues.at(i).toString();
-           continue;
-       }
-       QString currKey = (questionMark ? QString("?") : (key + QString("_") + QString::number(i)));
-       statement += ((i == 0) ? QString("") : QString(", "));
-       statement += currKey;
+        //       if ((type == in_select) || (type == not_in_select)) {
+        statement += listValues.at(i).toString() + ",";
+//        continue;
+        //       }
+        //       QString currKey = (questionMark ? QString("?") : (key + QString("_") + QString::number(i)));
+        //       statement += ((i == 0) ? QString("") : QString(", "));
+        //       statement += currKey;
     }
+    statement.chop(1);
 
     statement += QString(")");
     return statement;
